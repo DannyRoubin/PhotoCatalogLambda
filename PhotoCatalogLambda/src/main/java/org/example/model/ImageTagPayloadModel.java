2fdc23b6c540bbdf0691200f25fe16105117613d
Danny Roubin <danny.roubin@avanade.com>
2025-02-04 18:14:53 -0800

    fixed issue with model to properly insert tags into cosmos

--- PhotoCatalogLambda/src/main/java/org/example/model/ImageTagPayloadModel.java
+++ PhotoCatalogLambda/src/main/java/org/example/model/ImageTagPayloadModel.java
@@ -4,23 +4,31 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ImageTagPayloadModel {
+    private final String id;
     private String photoID;
     private String photoGUID;
     private String tags;
 
-    public ImageTagPayloadModel() {}
+    public ImageTagPayloadModel() {
+        this.id = null;
+    }
 
     @JsonCreator
     public ImageTagPayloadModel(
             @JsonProperty("photoID") String photoID,
             @JsonProperty("photoGUID") String photoGUID,
             @JsonProperty("tags") String tags) {
+        this.id = photoID;
         this.photoID = photoID;
         this.photoGUID = photoGUID;
         this.tags = tags;
     }
 
     // Getters
+    public String getId() {
+        return id;
+    }
+
     public String getPhotoID() {
         return photoID;
     }
@@ -33,6 +41,7 @@ public class ImageTagPayloadModel {
         return tags;
     }
 
+    // Setters (excluding `id` since it's final)
     public void setPhotoID(String photoID) {
         this.photoID = photoID;
     }
